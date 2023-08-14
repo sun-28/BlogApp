5fb994e6db9dcf76de1bd45258cc7d656710fc8b
sun-28 <114453652+sun-28@users.noreply.github.com>
2023-08-14 19:30:13 +0530

    Update UserPost.js

--- src/components/UserPost.js
+++ src/components/UserPost.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Post from './Post';
+import { host } from '../utils/ApiRoutes'
 import { format } from 'date-fns';
 const UserPost = () => {
     const navigate = useNavigate();
@@ -22,7 +23,7 @@ const UserPost = () => {
                 return (
                     <div className='post' key={index} onClick={() => navigate(`/post/${blog._id}`)}>
                         <div className='imgDiv'>
-                            < img className='postImg' src={`http://localhost:5000/${blog.cover}`} alt="IMAGE" />
+                            < img className='postImg' src={`${host}/${blog.cover}`} alt="IMAGE" />
                         </div>
                         <div className='contentDiv' >
                             <h2 className='postTitle' >{blog.title}</h2>
